5d966a03d4b357a685d171a4a961d97f8d1d07a1
Tom Atkins <treat@mac.com>
2024-09-29 02:10:49 -0400

    Initial Commit

---
+++ GuitarView/GuitarViewApp.swift
@@ -0,0 +1,33 @@
+//
+//  GuitarViewApp.swift
+//  GuitarView
+//
+//  Created by Tom Atkins on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GuitarViewApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
