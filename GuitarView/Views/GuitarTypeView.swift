dd2c5a1f6025b5efeb6cb974207ac90af0e28406
Tom Atkins <treat@mac.com>
2024-09-29 21:06:52 -0400

    Update GuitarTypeView.swift

--- GuitarView/Views/GuitarTypeView.swift
+++ GuitarView/Views/GuitarTypeView.swift
@@ -25,5 +25,5 @@ struct GuitarTypeView: View {
 }
 
 #Preview {
-    GuitarTypeView(guitarType: GuitarType(name: "visionOS", content: "Content, content and content. Ut necessitatibus voluptate praesentium id eos eaque itaque cumque. Sunt error et et. Dignissimos veritatis eum ad eius omnis. Pariatur eaque nihil fuga omnis quia. Aperiam corporis odit vero aspernatur in recusandae.Delectus quo sed dolores quo architecto et necessitatibus aut. Velit impedit animi est. Sapiente animi nostrum aperiam quod ut eos."))
+    GuitarTypeView(guitarType: GuitarType(name: "visionOS", content: "All about different kinds of guitars, and how to play them."))
 }
