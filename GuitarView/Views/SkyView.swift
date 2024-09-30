be448e0c453fc83b138829ad4833eef56b4dc267
Tom Atkins <treat@mac.com>
2024-09-29 20:32:09 -0400

    Added Skyview
    Added Skyview

---
+++ GuitarView/Views/SkyView.swift
@@ -0,0 +1,33 @@
+//
+//  SkyView.swift
+//  GuitarView
+//
+//  Created by Tom Atkins on 9/29/24.
+//
+
+import SwiftUI
+import RealityKit
+
+struct SkyView: View {
+    var body: some View {
+      RealityView { content in
+        guard let resource = try? await TextureResource(named: "skyView") else {
+          return
+        }
+        var material = UnlitMaterial()
+        material.color = .init(texture: .init(resource))
+        
+        let entity = Entity()
+        entity.components.set(ModelComponent(mesh: .generateSphere(radius: 1000), materials: [material]
+        ))
+        // point the texture inwards
+        entity.scale *= .init(x: -1, y: 1, z: 1)
+        
+        content.add(entity)
+      }
+    }
+}
+
+#Preview {
+    SkyView()
+}
